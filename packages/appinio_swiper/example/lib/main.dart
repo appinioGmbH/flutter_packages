bd9f10ef6caf9994d3380af37b6ac9ceafc70eab
Mujeeb khan <58891556+khanmujeeb687@users.noreply.github.com>
2023-11-21 16:52:29 +0100

    appinio_swiper - version 2.1.0
    * cleanup and notify on position change
    
    * finished?
    
    * formatting change
    
    * added comments to the example
    
    * moved documentation
    
    * added explanation to rotation point
    
    * fix angle and halve max angle to compensate
    
    * remove plural from cardsBuilder
    
    * switched all controller getters to nullable instead of throw
    
    * fixed animation interruption bugs, copied new swiper version content over
    
    * fix
    
    * fix
    
    * fix: onEnd
    
    * fix: allowUnswipe
    
    * Fix: canunswipe once
    
    * fix: onunswipe
    
    * onUnSwipe
    
    * fix: onSwipeCancelled
    
    * refactor: readme
    
    * readme
    
    * appinio_swiper - version 2.1.0
    
    ---------
    
    Co-authored-by: Casey Rogers <caseycrogers@berkeley.edu>
    Co-authored-by: Bilal <bilal.hammoud@appinio.com>

--- packages/appinio_swiper/example/lib/main.dart
+++ packages/appinio_swiper/example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:example/example_candidate_model.dart';
 import 'package:example/example_card.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'example_buttons.dart';
 
@@ -36,74 +37,124 @@ class Example extends StatefulWidget {
 
 class _ExamplePageState extends State<Example> {
   final AppinioSwiperController controller = AppinioSwiperController();
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 1)).then((_) {
+      _shakeCard();
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.75,
-            child: AppinioSwiper(
-              backgroundCardsCount: 3,
-              swipeOptions: const AppinioSwipeOptions.all(),
-              unlimitedUnswipe: true,
-              controller: controller,
-              unswipe: _unswipe,
-              onSwiping: (AppinioSwiperDirection direction) {
-                debugPrint(direction.toString());
-              },
-              onSwipe: _swipe,
-              padding: const EdgeInsets.only(
-                left: 25,
-                right: 25,
-                top: 50,
-                bottom: 40,
-              ),
-              onEnd: _onEnd,
-              cardsCount: candidates.length,
-              cardsBuilder: (BuildContext context, int index) {
-                return ExampleCard(candidate: candidates[index]);
-              },
+    return Material(
+      child: CupertinoPageScaffold(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                width: 80,
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .75,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 25,
+                  right: 25,
+                  top: 50,
+                  bottom: 40,
+                ),
+                child: AppinioSwiper(
+                  invertAngleOnBottomDrag: true,
+                  backgroundCardCount: 3,
+                  swipeOptions: const SwipeOptions.all(),
+                  controller: controller,
+                  onCardPositionChanged: (
+                    SwiperPosition position,
+                  ) {
+                    //debugPrint('${position.offset.toAxisDirection()}, '
+                    //    '${position.offset}, '
+                    //    '${position.angle}');
+                  },
+                  onSwipeEnd: _swipeEnd,
+                  onEnd: _onEnd,
+                  cardCount: candidates.length,
+                  cardBuilder: (BuildContext context, int index) {
+                    return ExampleCard(candidate: candidates[index]);
+                  },
+                ),
               ),
-              swipeLeftButton(controller),
-              const SizedBox(
-                width: 20,
+            ),
+            IconTheme.merge(
+              data: const IconThemeData(size: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TutorialAnimationButton(_shakeCard),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  swipeLeftButton(controller),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  swipeRightButton(controller),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  unswipeButton(controller),
+                ],
               ),
-              swipeRightButton(controller),
-              const SizedBox(
-                width: 20,
-              ),
-              unswipeButton(controller),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
 
-  void _swipe(int index, AppinioSwiperDirection direction) {
-    log("the card was swiped to the: " + direction.name);
-  }
-
-  void _unswipe(bool unswiped) {
-    if (unswiped) {
-      log("SUCCESS: card was unswiped");
-    } else {
-      log("FAIL: no card left to unswipe");
+  void _swipeEnd(int previousIndex, int targetIndex, SwiperActivity activity) {
+    switch (activity) {
+      case Swipe():
+        log('The card was swiped to the : ${activity.direction}');
+        log('previous index: $previousIndex, target index: $targetIndex');
+        break;
+      case Unswipe():
+        log('A ${activity.direction.name} swipe was undone.');
+        log('previous index: $previousIndex, target index: $targetIndex');
+        break;
+      case CancelSwipe():
+        log('A swipe was cancelled');
+        break;
+      case DrivenActivity():
+        log('Driven Activity');
+        break;
     }
   }
 
   void _onEnd() {
-    log("end reached!");
+    log('end reached!');
+  }
+
+  // Animates the card back and forth to teach the user that it is swipable.
+  Future<void> _shakeCard() async {
+    const double distance = 30;
+    // We can animate back and forth by chaining different animations.
+    await controller.animateTo(
+      const Offset(-distance, 0),
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeInOut,
+    );
+    await controller.animateTo(
+      const Offset(distance, 0),
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+    // We need to animate back to the center because `animateTo` does not center
+    // the card for us.
+    await controller.animateTo(
+      const Offset(0, 0),
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeInOut,
+    );
   }
 }
