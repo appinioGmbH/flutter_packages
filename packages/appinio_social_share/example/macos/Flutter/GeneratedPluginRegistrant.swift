b9f5c01351c4a8e0ee9f7a19745048203767b88c
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2026-02-16 15:53:26 +0100

    feat(social_share): expand iOS social media sharing and photo library access

--- packages/appinio_social_share/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/appinio_social_share/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
 }
